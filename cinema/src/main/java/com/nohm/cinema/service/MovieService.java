bca45ba49688f47045c2d4cf57cbadabea61d6a1
HMYIEN <bsm0407@naver.com>
2025-02-12 16:20:28 +0900

    [HM] 클래스 추가
    MovieService 클래스 생성

---
+++ cinema/src/main/java/com/nohm/cinema/service/MovieService.java
@@ -0,0 +1,15 @@
+package com.nohm.cinema.service;
+
+import com.nohm.cinema.aggregate.Genre;
+import com.nohm.cinema.repository.MemberRepository;
+
+public class MovieService {
+
+    private final MemberRepository mr = new MemberRepository();
+
+    public MovieService() {
+    }
+
+    public void searchMovie(Genre genre) {
+    }
+}
