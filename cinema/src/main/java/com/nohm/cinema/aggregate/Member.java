9ba47afb03bd5ea7acfdf8fb8f4ec90be5dd502e
SungMin-hub <sungmin806@gmail.com>
2025-02-12 17:05:49 +0900

    Member class commit
    기본 생성자 생성

--- cinema/src/main/java/com/nohm/cinema/aggregate/Member.java
+++ cinema/src/main/java/com/nohm/cinema/aggregate/Member.java
@@ -1,4 +1,18 @@
 package com.nohm.cinema.aggregate;
 
+import java.util.Date;
+
 public class Member {
+    private int memNo;
+    private String id;
+    private String pwd;
+    private Date birth;
+    private AccountStatus acntStatis;
+
+    public Member() {
+    }
+
+
+
+
 }
