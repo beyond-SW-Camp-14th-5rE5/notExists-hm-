e78c69c5261aa7215e9774ea7785a736d29fe1b4
50071 <rkd50071@gmail.com>
2025-02-13 18:04:15 +0900

    # 영화 더미 데이터 생성

--- cinema/src/main/java/com/nohm/cinema/repository/MovieRepository.java
+++ cinema/src/main/java/com/nohm/cinema/repository/MovieRepository.java
@@ -1,4 +1,62 @@
 package com.nohm.cinema.repository;
 
+import com.nohm.cinema.aggregate.Movie;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class MovieRepository {
+    private final ArrayList<Movie> movieList = new ArrayList<>();
+    private final File file = new File("src/main/java/com/nohm/cinema/db/movieDB.dat");
+
+    public MovieRepository() {
+        if (!file.exists()) {
+            ArrayList<Movie> defaultMovie = new ArrayList<>();
+            defaultMovie.add(new Movie());
+            defaultMovie.add(new Movie());
+            defaultMovie.add(new Movie());
+
+            saveMovies(defaultMovie);
+        }
+
+        loadMovies();
+    }
+
+    private void loadMovies() {
+        try (ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))) {
+            while (true) {
+                movieList.add((Movie) ois.readObject());
+            }
+        } catch (EOFException e) {
+            System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void saveMovies(ArrayList<Movie> inputMovies) {
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+            for (Movie mv : inputMovies) {
+                oos.writeObject(mv);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(oos != null) oos.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public ArrayList<Movie> selectAllMovies() {
+        return movieList;
+    }
 }
--- cinema/src/main/java/com/nohm/cinema/service/MovieService.java
+++ cinema/src/main/java/com/nohm/cinema/service/MovieService.java
@@ -1,18 +1,41 @@
 package com.nohm.cinema.service;
 
 import com.nohm.cinema.aggregate.Genre;
-import com.nohm.cinema.repository.MemberRepository;
+import com.nohm.cinema.aggregate.Member;
+import com.nohm.cinema.aggregate.Movie;
+import com.nohm.cinema.repository.MovieRepository;
+
+import java.util.ArrayList;
 
 public class MovieService {
 
-    private final MemberRepository mr = new MemberRepository();
+    private final MovieRepository mvr = new MovieRepository();
 
     public MovieService() {
+    }
+
+    public void findAllMovies() {
+        ArrayList<Movie> findMovies = mvr.selectAllMovies();
+
+        System.out.println("Service에서 조회 확인: ");
+        for (Movie mv : findMovies) {
+            System.out.println(mv);
+        }
+    }
+
+    public void findNowMovies() {
+
     }
 
     public void searchMovie(Genre genre) {
     }
 
-    public void findAllMovies() {
+    public void reserveMovie(Movie movie) {
+    }
+
+    public void findMemberForReservation(int memNo) {
+    }
+
+    public void cancelReserveMovie() {
     }
 }
